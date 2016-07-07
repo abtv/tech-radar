32702d21666ab3d664584f9c686656f0d7ebb94f
abtv <andreybutov@mail.ru>
2016-07-07 20:55:09 +0300

    forkme mobile icon

--- src/cljs/tech-radar/ui/navbar.cljs
+++ src/cljs/tech-radar/ui/navbar.cljs
@@ -6,22 +6,28 @@
             [tech-radar.services.search :refer [make-search]]))
 
 (defn brand-toggle [current-menu-item]
-  [:div.navbar-header {}
-   [:button.navbar-toggle {:type        "button"
-                           :data-toggle "collapse"
-                           :data-target ".navbar-ex1-collapse"}
-    [:span.sr-only {} "Toggle navigation"]
-    [:span.icon-bar {}]
-    [:span.icon-bar {}]
-    [:span.icon-bar {}]]
-   [:a.navbar-brand {:href "#/"} "Tech Radar"]
-   [:img {:src "images/radar.svg"}]
-   [:div.topic-container
-    [:span.topic-header current-menu-item]]
-   (when (= current-menu-item "")
-     [:a.fork-me {:href   "https://github.com/abtv/tech-radar"
-                  :target "_blank"}
-      [:img.fork-me-img {:src "images/forkme.png"}]])])
+  (let [github-link "https://github.com/abtv/tech-radar"]
+    [:div.navbar-header {}
+     [:button.navbar-toggle {:type        "button"
+                             :data-toggle "collapse"
+                             :data-target ".navbar-ex1-collapse"}
+      [:span.sr-only {} "Toggle navigation"]
+      [:span.icon-bar {}]
+      [:span.icon-bar {}]
+      [:span.icon-bar {}]]
+     [:a.navbar-brand {:href "#/"} "Tech Radar"]
+     [:img {:src "images/radar.svg"}]
+     [:div.topic-container
+      [:span.topic-header current-menu-item]]
+     (when (= current-menu-item "")
+       [:div.fork-me-mobile-wrapper
+        [:a.fork-me-mobile {:href   github-link
+                            :target "_blank"}
+         "Fork me on GitHub"]])
+     (when (= current-menu-item "")
+       [:a.fork-me-desktop {:href   github-link
+                            :target "_blank"}
+        [:img.fork-me-desktop-img {:src "images/forkme.png"}]])]))
 
 (defn records-per-page-settings [records-per-page set-record-count]
   [:ul.nav.navbar-right.top-nav.records-per-page {}
