f882ad743adb469dfe816579b56c5f42df9beeea
abtv <andreybutov@mail.ru>
2016-07-13 21:01:13 +0300

    tweets->bags-test

--- test/tech_radar/analytics/hype_meter_test.clj
+++ test/tech_radar/analytics/hype_meter_test.clj
@@ -33,3 +33,24 @@
     (is (< (abs (- (calc-similarity ws1 ws3) 0.333)) 0.001))
     (is (< (abs (- (calc-similarity ws1 ws5) 0.0)) 0.001))
     (is (< (abs (- (calc-similarity ws5 ws5) 0.0)) 0.001))))
+
+(defn tweets->bags [tweets stop-words-set]
+  (mapv (fn [{:keys [id text]}]
+          {:id    id
+           :words (utils/get-words text stop-words-set)}) tweets))
+
+(deftest tweets->bags-test
+  (let [stop-words #{"is" "and" "are" "a" "an"}
+        tweets     [{:id   1
+                     :text "Unit tests are cool"}
+                    {:id   2
+                     :text "Clojure is a language"}]]
+    (is (= [{:id    1
+             :words #{"unit" "tests" "cool"}}
+            {:id    2
+             :words #{"clojure" "language"}}] (tweets->bags tweets stop-words)))))
+
+(defn hype-meter [tweets])
+
+(deftest hype-meter-test
+  (let []))
